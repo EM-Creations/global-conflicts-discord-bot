8b148d593d69f7aba733f6cb1b64b5ef12dedd9b
VyMajoris <>
2023-10-05 19:11:49 -0300

    undo

--- src/helpers/restart_stop_command_perms_check.ts
+++ src/helpers/restart_stop_command_perms_check.ts
@@ -11,29 +11,19 @@ export default function permissionCheck(member) {
         (role) => role.id === process.env.DISCORD_ADMIN_ROLE_ID,
     );
 
-    if(isAdmin){
-        return true;
-    }
-
-
-    const isMissionReviewer = userRoleManager.cache.some(
-        (role) => role.id === process.env.DISCORD_MISSION_REVIEW_TEAM_ROLE_ID,
-    );
-    if (isMissionReviewer){
-        return true;
-    }
-    
     const isMissionMaker = userRoleManager.cache.some(
         (role) => role.id === process.env.DISCORD_MISSION_MAKER_ID,
     );
+    const isMissionReviewer = userRoleManager.cache.some(
+        (role) => role.id === process.env.DISCORD_MISSION_REVIEW_TEAM_ROLE_ID,
+    );
 
-    if (!isMissionMaker) {
+    if (!isMissionMaker && !isMissionReviewer && !isAdmin) {
         return 'You do not have permission to use this command!';
     }
-    
+
     if (isWeekend) {
         return 'This command only works on weekdays.';
     }
-
     return true;
 }
