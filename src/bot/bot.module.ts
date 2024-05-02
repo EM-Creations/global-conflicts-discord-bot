ffe6813709dc20e73e777d9384e9b4996fcbec2f
PiZZAD0X <509thparachuteinfantry@gmail.com>
2024-05-02 16:04:58 -0600

    Update bot.module.ts

--- src/bot/bot.module.ts
+++ src/bot/bot.module.ts
@@ -4,7 +4,7 @@ import { Module } from '@nestjs/common';
 
 import { TestServersCommands } from './commands/server/testservers.command';
 import { FantasyTestServerSubCommand } from './commands/subcommands/fantasy-modpack.subcommand';
-import { MainTestServerSubCommand } from './commands/subcommands/main-modpack.subcommand';
+import { MainTestServerSubCommand, MainServerSubCommand } from './commands/subcommands/main-modpack.subcommand';
 import { WW2TestServerSubCommand } from './commands/subcommands/ww2-modpack.subcomand';
 import { CrossCommunityTestServerSubCommand } from './commands/subcommands/crosscommunity-modpack.subcommand';
 import { ModRouletteTestServerSubCommand } from './commands/subcommands/mod-roulette-modpack.subcommand';
@@ -12,6 +12,6 @@ import { ModRouletteTestServerSubCommand } from './commands/subcommands/mod-roul
 @Module({
   imports: [DiscordModule.forFeature()],
   exports: [DiscordModule],
-  providers: [BotGateway, TestServersCommands, FantasyTestServerSubCommand, MainTestServerSubCommand, WW2TestServerSubCommand, CrossCommunityTestServerSubCommand, ModRouletteTestServerSubCommand],
+  providers: [BotGateway, TestServersCommands, FantasyTestServerSubCommand, MainTestServerSubCommand, MainServerSubCommand, WW2TestServerSubCommand, CrossCommunityTestServerSubCommand, ModRouletteTestServerSubCommand],
 })
 export class BotModule { }
