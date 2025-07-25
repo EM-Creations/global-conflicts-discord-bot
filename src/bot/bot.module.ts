388d3c5563af964bfd688dc66ee72273229dfc9e
Joe White <35034215+joewhite94@users.noreply.github.com>
2025-07-26 00:10:55 +0100

    missed a spot

--- src/bot/bot.module.ts
+++ src/bot/bot.module.ts
@@ -4,14 +4,9 @@ import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
 
 import { TestServersCommands } from './commands/server/testservers.command';
-import { FantasyTestServerSubCommand } from './commands/subcommands/fantasy-modpack.subcommand';
-import { MainTestServerSubCommand } from './commands/subcommands/main-modpack.subcommand';
 import { MainServerSubCommand } from './commands/subcommands/main.subcommand';
-import { WW2TestServerSubCommand } from './commands/subcommands/ww2-modpack.subcomand';
-import { CrossCommunityTestServerSubCommand } from './commands/subcommands/crosscommunity-modpack.subcommand';
-import { ModRouletteTestServerSubCommand } from './commands/subcommands/mod-roulette-modpack.subcommand';
+import { ConflictServerSubCommand } from './commands/subcommands/conflict.subcommand';
 import { StartTimeCommand } from './commands/subcommands/starttime.command';
-import { ReforgerServerSubCommand } from './commands/subcommands/reforger.subcommand';
 import { SwearJarModule } from '../swear-jar/swear-jar.module';
 import { VoiceRolesModule } from '../voice-roles/voice-roles.module';
 import { PonyBotListener } from '../PonyBot/PonyBot.listener';
@@ -29,12 +24,7 @@ import { PonyBotListener } from '../PonyBot/PonyBot.listener';
     BotGateway, 
     TestServersCommands, 
     MainServerSubCommand,
-    FantasyTestServerSubCommand, 
-    MainTestServerSubCommand, 
-    WW2TestServerSubCommand, 
-    CrossCommunityTestServerSubCommand, 
-    ModRouletteTestServerSubCommand,
-    ReforgerServerSubCommand,
+    ConflictServerSubCommand,
     StartTimeCommand,
     PonyBotListener,
 ],
